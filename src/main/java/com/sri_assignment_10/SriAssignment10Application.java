6bf2cdf85986e6f8ed8ca54c6fcb5923ff3d16a0
OSUMed <62990411+OSUMed@users.noreply.github.com>
2023-08-30 19:57:38 -0700

    first

---
+++ src/main/java/com/sri_assignment_10/SriAssignment10Application.java
@@ -0,0 +1,13 @@
+package com.sri_assignment_10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SriAssignment10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SriAssignment10Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sri_assignment_10/SriAssignment10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sri_assignment_10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SriAssignment10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
